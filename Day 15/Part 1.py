e7d561f397731b9cce5f64301aa82f988a4cc0f5
Alexey Dubrov <vhsw@ya.ru>
2018-12-28 12:56:36 +0300

    day 15

--- Day 15/Part 1.py
+++ Day 15/Part 1.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 from collections import deque
 
 
 class Unit:
-    def __init__(self, x, y, attack=3, health=200):
+    def __init__(self, x, y, attack_power=3, health=200):
         self.x = x
         self.y = y
-        self.attack = attack
+        self.attack_power = attack_power
         self.health = health
 
     @property
@@ -14,7 +14,7 @@ class Unit:
         return self.x, self.y
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.x}, {self.y}, {self.attack}, {self.health})'
+        return f'{self.__class__.__name__}({self.x}, {self.y}, {self.attack_power}, {self.health})'
 
     def __eq__(self, other):
         return self.coords == other.coords
@@ -23,19 +23,25 @@ class Unit:
         self.x = x
         self.y = y
 
+    def attack(self, other):
+        other.health -= self.attack_power
+
     def __sub__(self, other):
         return (abs(self.x - other.x) + abs(self.y - other.y))
 
 
 class Elf(Unit):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    pass
+    def __str__(self):
+        return f'E({self.health})'
+
 
 
 class Goblin(Unit):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    pass
 
+    def __str__(self):
+        return f'G({self.health})'
 
 class Board:
     def __init__(self, data):
@@ -54,22 +60,26 @@ class Board:
                 else:
                     row.append('.')
             self.grid.append(row)
+
         walls = []
         for i, line in enumerate(self.grid):
             for j, element in enumerate(line):
                 if element == '#':
                     walls.append((i, j))
-        self.walls = tuple(walls)
+        self.walls = set(walls)
 
     @property
     def units(self):
-        return sorted(self._units, key=lambda u: (u.x, u.y))
+        units = (u for u in self._units if u.health > 0)
+        return sorted(units, key=lambda u: (u.x, u.y))
 
     def get_targets(self, unit):
-        return tuple(other.coords for other in self.units if type(unit) != type(other))
+        return set(other.coords for other in self.units if type(unit) != type(other))
 
-    # def get_obstacles_pos(self, unit):
-    #     return tuple(other.coords for other in self.units if other != unit)
+    def obstacles(self, unit):
+        other_units = set(
+            other.coords for other in self.units if other != unit)
+        return self.walls.union(other_units)
 
     def __repr__(self):
         res = []
@@ -91,77 +101,95 @@ class Board:
         targets = self.get_targets(u)
         previous = {}
         distance = {}
-        to_visit = deque()
-        
-        for (dx, dy) in ((-1, 0), (0, -1), (0, +1), (+1, 0)):
-            new_coords = u.coords[0] + dx, u.coords[1] + dy
-            if new_coords not in self.walls:
-                to_visit.append(new_coords)
-                previous[new_coords] = u.coords
-                distance[new_coords] = 1
-        closest = None
 
+        to_visit = deque()
+        for (dx, dy) in ((-1, 0), (0, -1), (0, 1), (1, 0)):
+            new_coords = u.coords[0] + dx, u.coords[1] + dy
+            # if new_coords not in self.walls:
+            to_visit.append(new_coords)
+            previous[new_coords] = u.coords
+            distance[new_coords] = 1
+
+        closest = None
         while to_visit:
             coords = to_visit.popleft()
-            # if coords == (3, 15):
-            #     pass
-            if coords in self.walls:
-                continue
-            
+
             if coords in targets:
                 closest = coords
                 break
-            
+
+            if coords in self.obstacles(u):
+                continue
+
             for (dx, dy) in ((-1, 0), (0, -1), (0, +1), (+1, 0)):
                 new_coords = coords[0] + dx, coords[1] + dy
                 if new_coords == (3, 15):
                     pass
-                if new_coords not in previous and new_coords not in self.walls:
+                if new_coords not in previous:
                     previous[new_coords] = coords
                     distance[new_coords] = distance[coords] + 1
                     to_visit.append(new_coords)
 
         if closest is None:
-            return None
+            return None, -1
 
         position = closest
         next_move = previous[closest]
-        _dist = 0
-        trace =[next_move]
+        # _dist = 0
+        trace = [next_move]
         while next_move != u.coords:
-            _dist += 1
+            # _dist += 1
             trace.append(next_move)
-            position = next_move 
+            position = next_move
             next_move = previous[position]
-        print(trace)
-        print(u, distance[closest], closest)
-        return position 
+        # print(trace)
+        # print(u, distance[closest], closest)
+        return position, distance[closest]
 
+    def get_neighbour_enemies(self, unit):
+        x, y = unit.coords
+        neighbours = []
+        for (dx, dy) in ((-1, 0), (0, -1), (0, 1), (1, 0)):
+            new_coord = x + dx, y + dy
+            for other in self.units:
+                if new_coord == other.coords:
+                    if not isinstance(other, unit.__class__):
+                        neighbours.append(other)
+        return neighbours
 
     def step(self):
+        move_success = False
         for u in self.units:
+            if u.health <= 0:
+                continue
             coord, distance = self.get_closest(u)
             if distance > 1:
                 u.move(*coord)
                 distance -= 1
             if distance == 1:
-                pass
-
-
+                targets = self.get_neighbour_enemies(u)
+                selected_target = min(targets, key=lambda t: t.health)
+                u.attack(selected_target)
+            move_success = move_success or coord is not None
+        return move_success
 
 def madness(path):
     with open(path) as f:
         raw_data = f.read().splitlines()
     board = Board(raw_data)
-    while True:
+    step = -1
+    while board.step():
+        step += 1
+        print(step)
         print(board)
         import time
-        time.sleep(0.5)
-        board.step()
+        # time.sleep(0.05)
 
-    return
+    return sum(u.health for u in board.units) * step
 
 
-# assert madness('Day 15/example.0.txt') == (0, 3)
-# assert madness('Day 15/example.1.txt') == (7, 3)
+# assert madness('Day 15/example.0.txt') == 27730
+assert madness('Day 15/example.1.txt') == 36334
+assert madness('Day 15/example.2.txt') == 39514
+assert madness('Day 15/example.3.txt') == 27755
 print(madness('Day 15/input.txt'))
