47b1d8081ecde9f144d8f4295197700d59ba4372
Alexey Dubrov <vhsw@ya.ru>
2019-12-22 07:48:22 +0300

    day 18

--- 2019/Day 18/test_many_worlds_interpretation.py
+++ 2019/Day 18/test_many_worlds_interpretation.py
@@ -1,7 +1,24 @@
-"""Day N Tests"""
+"""Day 18 Tests"""
 
 import pytest
-from many_worlds_interpretation import part1, part2
+from many_worlds_interpretation import shortest_path_length, part1, part2
+
+
+@pytest.mark.parametrize(
+    "maze, length",
+    [
+        [
+            """########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################""",
+            81,
+        ],
+    ],
+)
+def test_path_len(maze, length):
+    assert shortest_path_length(maze) == length
 
 
 def test_parts():
