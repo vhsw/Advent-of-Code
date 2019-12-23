bb8e6a4189932c3229458426ff7ef5cebcf0a619
Alexey Dubrov <vhsw@ya.ru>
2019-12-23 21:33:01 +0300

    day 18

--- 2019/Day 18/test_many_worlds_interpretation.py
+++ 2019/Day 18/test_many_worlds_interpretation.py
@@ -1,7 +1,9 @@
 """Day 18 Tests"""
 
 import pytest
-from many_worlds_interpretation import shortest_path_length, part1, part2
+
+from many_worlds_interpretation_part_1 import shortest_path_length as sp1, part1
+from many_worlds_interpretation_part_2 import shortest_path_length as sp2, part2
 
 
 @pytest.mark.parametrize(
@@ -18,7 +20,48 @@ from many_worlds_interpretation import shortest_path_length, part1, part2
     ],
 )
 def test_path_len(maze, length):
-    assert shortest_path_length(maze) == length
+    assert sp1(maze) == length
+
+
+@pytest.mark.parametrize(
+    "maze, length",
+    [
+        [
+            """###############
+#d.ABC.#.....a#
+######...######
+######.@.######
+######...######
+#b.....#.....c#
+###############""",
+            24,
+        ],
+        [
+            """#############
+#DcBa.#.GhKl#
+#.###...#I###
+#e#d#.@.#j#k#
+###C#...###J#
+#fEbA.#.FgHi#
+#############""",
+            32,
+        ],
+        [
+            """#############
+#g#f.D#..h#l#
+#F###e#E###.#
+#dCba...BcIJ#
+#####.@.#####
+#nK.L...G...#
+#M###N#H###.#
+#o#m..#i#jk.#
+#############""",
+            72,
+        ],
+    ],
+)
+def test_path_len_2(maze, length):
+    assert sp2(maze) == length
 
 
 def test_parts():
