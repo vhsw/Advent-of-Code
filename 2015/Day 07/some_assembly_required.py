d8203f59b7ee9650bb897b5601f540ff2c6230d1
Alexey Dubrov <vhsw@ya.ru>
2020-12-02 00:35:35 +0300

    part 1

--- 2015/Day 07/some_assembly_required.py
+++ 2015/Day 07/some_assembly_required.py
@@ -1,6 +1,7 @@
 "Day 07 answers"
-import networkx as nx
 import re
+
+import numpy as np
 
 INPUT = "2015/Day 07/input.txt"
 
@@ -8,9 +9,9 @@ BINOPS = "AND|OR|LSHIFT|RSHIFT".split("|")
 
 
 def parse(line):
-    load = r"(\w+) -> (\w+)"
-    unop = r"(NOT) (\w+) -> (\w+)"
-    binop = r"(\w+) ?(AND|OR|LSHIFT|RSHIFT) (\w+) -> (\w+)"
+    load = r"^(\w+) -> (\w+)"
+    unop = r"^(NOT) (\w+) -> (\w+)"
+    binop = r"^(\w+) ?(AND|OR|LSHIFT|RSHIFT) (\w+) -> (\w+)"
     if m := re.match(binop, line):
         in1, op, in2, out = m.groups()
         if in1.isnumeric():
@@ -24,21 +25,53 @@ def parse(line):
             in1 = int(in1)
         return op, in1, out
     if m := re.match(load, line):
-        op, in1, out = m.groups()
+        in1, out = m.groups()
         if in1.isnumeric():
             in1 = int(in1)
         return "LOAD", in1, out
 
 
+def do(op, ins):
+    ins = np.array(ins, dtype="uint16")
+    # print(op, ins)
+    if op == "LOAD":
+        return ins[0]
+    if op == "NOT":
+        return ~ins[0]
+    if op == "AND":
+        return ins[0] & ins[1]
+    if op == "OR":
+        return ins[0] | ins[1]
+    if op == "LSHIFT":
+        return ins[0] << ins[1]
+    if op == "RSHIFT":
+        return ins[0] >> ins[1]
+
+
 def part1(data):
     "Part 1 answer"
-
-    G = nx.DiGraph()
+    lst = []
+    wires = {}
     for line in data:
         op, *ins, out = parse(line)
+        lst.append([op, ins, out])
+        wires[out] = None
         for i in ins:
-            G.add_edge(i, out, op=op)
-    nx.draw(G, with_labels=True)
+            if isinstance(i, str):
+                wires[i] = None
+            else:
+                wires[i] = i
+    todo = lst
+    while todo:
+        for op, ins, out in todo:
+            if not all(wires[i] is not None for i in ins):
+                continue
+            res = do(op, [wires[i] for i in ins])
+            if res is not None:
+                if out == "a":
+                    return res
+                wires[out] = res
+        todo = [[op, ins, out] for op, ins, out in lst if wires[out] is None]
 
 
 def part2(data):
@@ -48,5 +81,14 @@ def part2(data):
 if __name__ == "__main__":
     with open(INPUT) as fp:
         DATA = fp.readlines()
+    #     DATA = """123 -> x
+    # 456 -> y
+    # h -> a
+    # x AND y -> d
+    # x OR y -> e
+    # x LSHIFT 2 -> f
+    # y RSHIFT 2 -> g
+    # NOT x -> h
+    # NOT y -> i""".splitlines()
     print(f"Part 1: { part1(DATA) }")
     print(f"Part 2: { part2(DATA) }")
