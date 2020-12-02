db27428b558ff067ceb8a11af0d455e2df1c4317
Alexey Dubrov <vhsw@ya.ru>
2020-12-02 13:17:50 +0300

    day 9

---
+++ 2015/Day 09/all_in_a_single_night.py
@@ -0,0 +1,39 @@
+"Day 09 answers"
+import re
+import networkx as nx
+from itertools import permutations
+
+INPUT = "2015/Day 09/input.txt"
+
+
+def part1(data):
+    "Part 1 answer"
+    G = nx.Graph()
+    for line in data:
+        match = re.match(r"(\w+) to (\w+) = (\d+)", line)
+        src, dst, dist = match.groups()
+        G.add_edge(src, dst, weight=int(dist))
+    dists = []
+    for p in permutations(G.nodes):
+        dists.append(sum(G[src][dst]["weight"] for src, dst in zip(p, p[1:])))
+    return min(dists)
+
+
+def part2(data):
+    "Part 2 answer"
+    G = nx.Graph()
+    for line in data:
+        match = re.match(r"(\w+) to (\w+) = (\d+)", line)
+        src, dst, dist = match.groups()
+        G.add_edge(src, dst, weight=int(dist))
+    dists = []
+    for p in permutations(G.nodes):
+        dists.append(sum(G[src][dst]["weight"] for src, dst in zip(p, p[1:])))
+    return max(dists)
+
+
+if __name__ == "__main__":
+    with open(INPUT) as fp:
+        DATA = fp.readlines()
+    print(f"Part 1: { part1(DATA) }")
+    print(f"Part 2: { part2(DATA) }")
