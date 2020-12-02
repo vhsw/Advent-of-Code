8165c7f3e4ddc13bc35936f91bc47863673ea942
Alexey Dubrov <vhsw@ya.ru>
2020-12-02 12:49:07 +0300

    part2

--- 2015/Day 08/matchsticks.py
+++ 2015/Day 08/matchsticks.py
@@ -1,18 +1,15 @@
 "Day 08 answers"
-from typing import Tuple
-
 
 INPUT = "2015/Day 08/input.txt"
 
 
-def part1(data: Tuple[str, ...]):
+def part1(data):
     "Part 1 answer"
     chars = 0
     mem = 0
     for line in data:
-        print(repr(line))
-        print(repr(bytes(line, "utf-8").decode("unicode_escape")))
-        chars += len([i for i in line if i not in {" ", "\n"}])
+        line = line.strip()
+        chars += len(line)
         mem += len(bytes(line, "utf-8").decode("unicode_escape").replace("\n", "")) - 2
 
     return chars - mem
@@ -21,24 +18,18 @@ def part1(data: Tuple[str, ...]):
 def part2(data):
     "Part 2 answer"
     chars = 0
-    mem = 0
+    esc = 0
     for line in data:
-        print(repr(line))
-        print(repr(bytes(line, "utf-8").decode("unicode_escape")))
-        chars += len([i for i in line if i not in {" ", "\n"}])
-        mem += len(bytes(line, "utf-8").decode("unicode_escape").replace("\n", "")) - 2
+        line = line.strip()
+        escaped = '"' + line.replace("\\", "\\\\").replace('"', r"\"") + '"'
+        esc += len(escaped)
+        chars += len(line)
 
-    return chars - mem
+    return esc - chars
 
 
 if __name__ == "__main__":
     with open(INPUT) as fp:
         DATA = fp.readlines()
-        DATA = r"""
-""
-"abc"
-"aaa\"aaa"
-"\x27"
-""".strip().splitlines()
     print(f"Part 1: { part1(DATA) }")
     print(f"Part 2: { part2(DATA) }")
