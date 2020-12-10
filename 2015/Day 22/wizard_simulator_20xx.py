5b40fc5cfa3e255042a9ce44a7266f1c0e80a36b
Alexey Dubrov <vhsw@ya.ru>
2020-12-10 09:44:37 +0300

    day 22 template

---
+++ 2015/Day 22/wizard_simulator_20xx.py
@@ -0,0 +1,17 @@
+"Day 22 answers"
+INPUT = "2015/Day 22/input.txt"
+
+
+def part1(data):
+    "Part 1 answer"
+
+
+def part2(data):
+    "Part 2 answer"
+
+
+if __name__ == "__main__":
+    with open(INPUT) as fp:
+        DATA = fp.read()
+    print(f"Part 1: { part1(DATA) }")
+    print(f"Part 2: { part2(DATA) }")
