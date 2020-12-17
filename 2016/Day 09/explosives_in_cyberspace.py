2fe74e00668e2272ea8068dfc3530128c7d7681e
Alexey Dubrov <vhsw@ya.ru>
2020-12-17 23:05:00 +0300

    day 09

--- 2016/Day 09/explosives_in_cyberspace.py
+++ 2016/Day 09/explosives_in_cyberspace.py
@@ -5,45 +5,45 @@ INPUT = "2016/Day 09/input.txt"
 
 
 def decompress(data):
-    i = 0
+    idx = 0
     result = ""
-    while i < len(data):
+    while idx < len(data):
         # print(f"{i=}")
-        if data[i] == "(":
+        if data[idx] == "(":
             buf = ""
-            i += 1
-            while data[i] != ")":
-                buf += data[i]
-                i += 1
-            i += 1
-            length, times = map(int, re.match("(\d+)x(\d+)", buf).groups())
-            result += data[i : i + length] * times
-            i += length
+            idx += 1
+            while data[idx] != ")":
+                buf += data[idx]
+                idx += 1
+            idx += 1
+            length, times = map(int, re.match(r"(\d+)x(\d+)", buf).groups())
+            result += data[idx : idx + length] * times
+            idx += length
         else:
-            result += data[i]
-            i += 1
+            result += data[idx]
+            idx += 1
 
     # print(f"{result=}")
     return result
 
 
-def decomp_rec(data):
-    i = 0
+def decompress_rec(data):
+    idx = 0
     result_length = 0
-    while i < len(data):
-        if data[i] == "(":
+    while idx < len(data):
+        if data[idx] == "(":
             buf = ""
-            i += 1
-            while data[i] != ")":
-                buf += data[i]
-                i += 1
-            i += 1
-            length, times = map(int, re.match("(\d+)x(\d+)", buf).groups())
-            result_length += decomp_rec(data[i : i + length]) * times
-            i += length
+            idx += 1
+            while data[idx] != ")":
+                buf += data[idx]
+                idx += 1
+            idx += 1
+            length, times = map(int, re.match(r"(\d+)x(\d+)", buf).groups())
+            result_length += decompress_rec(data[idx : idx + length]) * times
+            idx += length
         else:
             result_length += 1
-            i += 1
+            idx += 1
 
     return result_length
 
@@ -55,13 +55,11 @@ def part1(data):
 
 def part2(data):
     "Part 2 answer"
-    return decomp_rec(data)
+    return decompress_rec(data)
 
 
 if __name__ == "__main__":
     with open(INPUT) as fp:
         DATA = fp.read().strip()
-
-    # DATA = "(27x12)(20x12)(13x14)(7x10)(1x12)A"
     print(f"Part 1: { part1(DATA) }")
     print(f"Part 2: { part2(DATA) }")
