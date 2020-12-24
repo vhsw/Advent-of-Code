69258274886eae47e23dfcfc546d9283898b3ae5
Alexey Dubrov <vhsw@ya.ru>
2020-12-25 02:34:56 +0300

    day 24

--- 2020/Day 24/lobby_layout.py
+++ 2020/Day 24/lobby_layout.py
@@ -1,4 +1,7 @@
 "Day 24 answers"
+from time import sleep
+from typing import Dict, List, Set, Tuple
+
 INPUT = "2020/Day 24/input.txt"
 
 
@@ -7,7 +10,7 @@ def parse(line):
     directions = []
     while idx < len(line):
         a = line[idx]
-        b = " "
+        b = ""
         if idx < len(line) - 1:
             b = line[idx + 1]
         if a + b in ["se", "sw", "nw", "ne"]:
@@ -35,7 +38,7 @@ def parse(line):
 #        \       /     \       /
 #         \_____/       \_____/
 
-MOVES = {
+MOVES: Dict[str, Tuple[int, int]] = {
     "nw": (1, -1),
     "ne": (1, 0),
     "w": (0, -1),
@@ -45,7 +48,7 @@ MOVES = {
 }
 
 
-def move(directions):
+def move(directions: List[str]):
     x, y = 0, 0
     for direction in directions:
         dx, dy = MOVES[direction]
@@ -54,44 +57,71 @@ def move(directions):
     return x, y
 
 
-def part1(data):
-    "Part 1 answer"
-    tiles = {}
+def tile_floor(data: List[str]):
+    tiles: Set[Tuple[int, int]] = set()
     for line in data:
         directions = parse(line)
-        x, y = move(directions)
-        tiles[x, y] = not tiles.setdefault((x, y), True)
-    return sum(not t for t in tiles.values())
+        tile = move(directions)
+        if tile in tiles:
+            tiles.remove(tile)
+        else:
+            tiles.add(tile)
+    return tiles
 
 
-def part2(data):
+def part1(data: List[str]):
+    "Part 1 answer"
+    tiles = tile_floor(data)
+    return len(tiles)
+
+
+def neighbors(x, y):
+    for dx, dy in MOVES.values():
+        yield x + dx, y + dy
+
+
+def display(tiles):
+    size = max(max(abs(t[0]), abs(t[1])) for t in tiles)
+    if size % 2 == 0:
+        size += 1
+    lines = []
+    for y in range(-size, size + 1):
+        line = " " if y % 2 == 0 else ""
+        line += ". " * ((size - y) // 2)
+        for x in range(-size, size + 1):
+            x = -x
+            if x == y == 0:
+                line += "x "
+                continue
+            line += "# " if (x, y) in tiles else ". "
+
+        line += ". " * ((size + y) // 2)
+        lines.append(line)
+    print("\n".join(lines) + "\n")
+    sleep(0.1)
+
+
+def part2(data: List[str], make_show=False):
     "Part 2 answer"
+    tiles = tile_floor(data)
+    for _ in range(100):
+        tmp = set()
+        size = max(max(abs(t[0]), abs(t[1])) for t in tiles) + 1
+        for x in range(-size, size + 1):
+            for y in range(-size, size + 1):
+                tile = x, y
+                s = sum(neighbor in tiles for neighbor in neighbors(*tile))
+                if s == 2 or tile in tiles and s == 1:
+                    tmp.add(tile)
+        tiles = tmp
+        size += 1
+        if make_show:
+            display(tiles)
+    return len(tiles)
 
 
 if __name__ == "__main__":
     with open(INPUT) as fp:
-        DATA = fp.read().strip().split()
-    DATA1 = """
-sesenwnenenewseeswwswswwnenewsewsw
-neeenesenwnwwswnenewnwwsewnenwseswesw
-seswneswswsenwwnwse
-nwnwneseeswswnenewneswwnewseswneseene
-swweswneswnenwsewnwneneseenw
-eesenwseswswnenwswnwnwsewwnwsene
-sewnenenenesenwsewnenwwwse
-wenwwweseeeweswwwnwwe
-wsweesenenewnwwnwsenewsenwwsesesenwne
-neeswseenwwswnwswswnw
-nenwswwsewswnenenewsenwsenwnesesenew
-enewnwewneswsewnwswenweswnenwsenwsw
-sweneswneswneneenwnewenewwneswswnese
-swwesenesewenwneswnwwneseswwne
-enesenwswwswneneswsenwnewswseenwsese
-wnwnesenesenenwwnenwsewesewsesesew
-nenewswnwewswnenesenwnesewesw
-eneswnwswnwsenenwnwnwwseeswneewsenese
-neswnwewnwnwseenwseesewsenwsweewe
-wseweeenwnesenwwwswnew""".strip().split()
-    # DATA = ["esenee"]
+        DATA = fp.read().strip().split("\n")
     print(f"Part 1: { part1(DATA) }")
-    print(f"Part 2: { part2(DATA) }")
+    print(f"Part 2: { part2(DATA, make_show=False) }")
