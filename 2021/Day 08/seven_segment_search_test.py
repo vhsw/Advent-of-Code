7f9a5ebf15c1d6ea449d36799a5b30aee617db31
Alexey Dubrov <nevermind1025@gmail.com>
2021-12-08 09:45:20 +0300

    day 08

---
+++ 2021/Day 08/seven_segment_search_test.py
@@ -0,0 +1,27 @@
+"""Day 8: tests"""
+from seven_segment_search import DATA, part1, part2
+
+EXAMPLE = """
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+""".strip()
+
+
+def test_part1():
+    """Part 1 test"""
+    assert part1(EXAMPLE) == 26
+    assert part1(DATA) == 365
+
+
+def test_part2():
+    """Part 2 test"""
+    assert part2(EXAMPLE) == 61229
+    assert part2(DATA) == 975706
