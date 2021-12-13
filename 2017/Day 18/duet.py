044cc6b3ebd648dd135960e78c4ef3443a77b8c1
Alexey Dubrov <nevermind1025@gmail.com>
2021-12-13 19:38:55 +0300

    day 18

--- 2017/Day 18/duet.py
+++ 2017/Day 18/duet.py
@@ -1,6 +1,7 @@
 """Day 18: Duet"""
 from collections import deque
 from string import ascii_lowercase
+from typing import Callable
 
 with open("2017/Day 18/input.txt", encoding="utf-8") as fp:
     DATA = fp.read().strip()
@@ -44,48 +45,51 @@ def part2(data: str):
     feed = data.splitlines()
     q_0: deque[int] = deque()
     q_1: deque[int] = deque()
-    count = 0
 
-    def evaluate(program_id: int, rx: deque[int], tx: deque[int], feed: list[str]):
-        nonlocal count
-        reg = {l: 0 for l in ascii_lowercase}
-        reg["p"] = program_id
+    return sum(
+        out[1] for out in zip(evaluate(0, q_0, q_1, feed), evaluate(1, q_1, q_0, feed))
+    )
 
-        def value(arg: str):
-            if arg.isalpha():
-                return reg[arg]
-            return int(arg)
 
-        ip = 0
-        while ip < len(feed):
-            op, arg, *rest = feed[ip].split()
-            match op:
-                case "snd":
-                    if program_id == 1:
-                        count += 1
-                    tx.append(value(arg))
-                case "set":
-                    reg[arg] = value(rest[0])
-                case "add":
-                    reg[arg] += value(rest[0])
-                case "mul":
-                    reg[arg] *= value(rest[0])
-                case "mod":
-                    reg[arg] %= value(rest[0])
-                case "rcv" if arg != "0":
-                    if not rx:
-                        yield "WAIT"
-                    if not rx:
-                        return
-                    val = rx.popleft()
-                    reg[arg] = val
-                case "jgz" if value(arg) > 0:
-                    ip += value(rest[0]) - 1
-            ip += 1
+def evaluate(
+    program_id: int,
+    rx: deque[int],
+    tx: deque[int],
+    feed: list[str],
+):
+    reg = {l: 0 for l in ascii_lowercase}
+    reg["p"] = program_id
 
-    for _ in zip(evaluate(0, q_0, q_1, feed), evaluate(1, q_1, q_0, feed)):
-        pass
-    return count
+    def value(arg: str):
+        if arg.isalpha():
+            return reg[arg]
+        return int(arg)
+
+    ip = 0
+    while ip < len(feed):
+        op, arg, *rest = feed[ip].split()
+        match op:
+            case "snd":
+                tx.append(value(arg))
+                yield 1
+            case "set":
+                reg[arg] = value(rest[0])
+            case "add":
+                reg[arg] += value(rest[0])
+            case "mul":
+                reg[arg] *= value(rest[0])
+            case "mod":
+                reg[arg] %= value(rest[0])
+            case "rcv" if arg != "0":
+                if not rx:
+                    yield 0
+                if not rx:
+                    return
+                val = rx.popleft()
+                reg[arg] = val
+            case "jgz" if value(arg) > 0:
+                ip += value(rest[0]) - 1
+        ip += 1
 
 
 if __name__ == "__main__":
