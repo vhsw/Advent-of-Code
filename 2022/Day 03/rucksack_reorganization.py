b278e7feff981fe462049639b2e5dce8ecea4573
Alexey Dubrov <nevermind1025@gmail.com>
2022-12-03 09:24:27 +0300

    day 03

---
+++ 2022/Day 03/rucksack_reorganization.py
@@ -0,0 +1,41 @@
+"""Day 3: Rucksack Reorganization"""
+
+with open("2022/Day 03/input.txt", encoding="utf-8") as fp:
+    DATA = fp.read().strip()
+
+
+def part1(data: str):
+    """Part 1 solution"""
+    score = 0
+    for line in data.splitlines():
+        l = len(line)
+        diff = set(line[: l // 2]) & set(line[l // 2 :])
+        d = diff.pop()
+        score += priority(d)
+    return score
+
+
+def part2(data: str):
+    """Part 2 solution"""
+    lines = data.splitlines()
+    score = 0
+    for i in range(0, len(lines), 3):
+        diff = set(lines[i]) & set(lines[i + 1]) & set(lines[i + 2])
+        if len(diff) != 1:
+            continue
+        d = diff.pop()
+        score += priority(d)
+
+    return score
+
+
+def priority(d: str):
+    if "a" <= d <= "z":
+        return ord(d) - ord("a") + 1
+    else:
+        return ord(d) - ord("A") + 27
+
+
+if __name__ == "__main__":
+    print(f"Part 1: { part1(DATA) }")
+    print(f"Part 2: { part2(DATA) }")
---
+++ 2022/Day 03/rucksack_reorganization_test.py
@@ -0,0 +1,23 @@
+"""Day 3: tests"""
+from rucksack_reorganization import DATA, part1, part2
+
+EXAMPLE = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+""".strip()
+
+
+def test_part1():
+    """Part 1 test"""
+    assert part1(EXAMPLE) == 157
+    assert part1(DATA) == 7428
+
+
+def test_part2():
+    """Part 2 test"""
+    assert part2(EXAMPLE) == 70
+    assert part2(DATA) == 0
